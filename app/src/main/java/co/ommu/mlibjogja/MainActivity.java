7495f0e94392d4d842da73f97f14d16a58e6e28b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-17 03:27:55 +0700

    simple search: fixed

--- app/src/main/java/co/ommu/mlibjogja/MainActivity.java
+++ app/src/main/java/co/ommu/mlibjogja/MainActivity.java
@@ -1,15 +1,27 @@
 package co.ommu.mlibjogja;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //SettingFunction setting = new SettingFunction();
+    Spinner spinnerSearch;
+    EditText fieldSearch;
+    Button buttonSearch;
+    int selected;
+    String[] searching = {"title", "publisher", "author", "subject"};
+    String keyword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +29,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //setting.onCreateOptionsMenu(Menu this);
+        //setting.onOptionsItemSelected(MenuItem this);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        spinnerSearch = (Spinner) findViewById(R.id.spinnerSearch);
+        fieldSearch = (EditText) findViewById(R.id.fieldSearch);
+        buttonSearch = (Button) findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(this);
+
+        spinnerSearch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long arg3) {
+                // TODO Auto-generated method stub
+                selected = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
             }
         });
     }
@@ -43,10 +66,32 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.actionSimple) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        } else if (id == R.id.actionAdvance) {
+            startActivity(new Intent(getApplicationContext(), AdvanceSearchActivity.class));
+        } else if (id == R.id.actionSetting) {
+            startActivity(new Intent(getApplicationContext(), SettingActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View arg0) {
+        int id = arg0.getId();
+
+        if (id == R.id.buttonSearch) {
+            // TODO Auto-generated method stub
+            Intent intent = new Intent(getBaseContext(), ResultActivity.class);
+            keyword = fieldSearch.getText().toString();
+            if (keyword != null && keyword.length() > 0) {
+                intent.putExtra("url", searching[selected] + "/" + keyword);
+                intent.putExtra("keyword", keyword);
+                startActivity(intent);
+                fieldSearch.getText().clear();
+            } else
+                Toast.makeText(getBaseContext(), "Pencarian tidak boleh kosong", Toast.LENGTH_SHORT).show();
+        }
     }
 }
