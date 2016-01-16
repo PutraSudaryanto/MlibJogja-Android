2efe3811b6fe7c623e09f782131fad6fdab9c210
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-17 03:29:34 +0700

    setting: add source

---
+++ app/src/main/java/co/ommu/mlibjogja/SettingFunction.java
@@ -0,0 +1,37 @@
+package co.ommu.mlibjogja;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Putra Sudaryanto on 1/16/2016.
+ */
+public class SettingFunction extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.actionSimple) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        } else if (id == R.id.actionAdvance) {
+            startActivity(new Intent(getApplicationContext(), AdvanceSearchActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
